a0c32a3cbcdedc7382c8c75d4dba96fafaf2b71c
8600913764 <patilakshay8649@gmail.com>
2026-03-08 15:03:11 +0530

    first-commit

---
+++ BootProj10-DirectValueInjectionUsingValueAnnotation/src/main/java/com/akshay/BootProj10DirectValueInjectionUsingValueAnnotationWithSpelApplication.java
@@ -0,0 +1,22 @@
+package com.akshay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.akshay.sbeans.InfoService;
+
+@SpringBootApplication
+public class BootProj10DirectValueInjectionUsingValueAnnotationWithSpelApplication {
+
+	public static void main(String[] args) {
+	   
+		try(ConfigurableApplicationContext ctx =
+				SpringApplication.run(BootProj10DirectValueInjectionUsingValueAnnotationWithSpelApplication.class, args);){
+			
+			InfoService infoService = ctx.getBean(InfoService.class);
+			infoService.displayInfo();
+		}
+	}
+
+}
